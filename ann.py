3f7fa0971a63658c81c880198a392a5da8520ebd
manqjayak <komangjaya453@gmail.com>
2023-11-16 22:44:23 +0800

    best accuracy for ann and svm so far

--- ann.py
+++ ann.py
@@ -1,4 +1,5 @@
 # Importing necessary libraries
+import time
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -8,6 +9,7 @@ from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score
 from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import StandardScaler
 # Load data from CSV file
 data = pd.read_csv('StressLevelDataset.csv')  # Replace 'your_data.csv' with the actual file path
 
@@ -18,40 +20,94 @@ x = data.iloc[:,:-1]
 y = data['stress_level'].values
 print(x)
 print(y)
+# feature selection with corelation
+# column_drop = ['self_esteem',
+#                'sleep_quality',
+#                'living_conditions',
+#                'safety',
+#                'basic_needs',
+#                'academic_performance',
+#                'teacher_student_relationship',
+#                'social_support',
+#                'headache']
+# x = x.drop(columns = column_drop)
 
 # Convert y to one-hot encoding for multiclass classification
 y_onehot = to_categorical(y, num_classes=3)
 
 print(f"len of dependent variable {len(x.columns.tolist())}")
+# scaler = StandardScaler()
+# X_standardized = scaler.fit_transform(x)
 # Splitting the data into training and testing sets (70% train, 30% test)
 X_train, X_test, y_train, y_test = train_test_split(x, y_onehot, test_size=0.3, random_state=42)
 print("--")
 print(X_test)
 print("--")
 
-# Creating a neural network model with 6 hidden layers
+
+######################################################################
+# Measure the training time
+start_time = time.time()
+
+# Creating a neural network model
 model = Sequential()
 
 # Adding the input layer
 model.add(Dense(units=20, input_dim=X_train.shape[1], activation='relu'))  # 20 neurons in the first hidden layer
 
-# # Adding 5 hidden layers
+# # Adding N hidden layers
 # for _ in range(8):
 #     model.add(Dense(units=30, activation='relu'))  # 10 neurons in each hidden layer
-model.add(Dense(units=120, activation='relu'))  
-model.add(Dense(units=120, activation='relu'))  
-model.add(Dense(units=110, activation='relu'))  
-model.add(Dense(units=110, activation='relu'))  
-model.add(Dense(units=100, activation='relu'))  
-model.add(Dense(units=80, activation='relu'))  
-model.add(Dense(units=80, activation='relu'))  
-model.add(Dense(units=80, activation='relu'))  
-model.add(Dense(units=60, activation='relu'))  
-model.add(Dense(units=40, activation='relu'))  
-model.add(Dense(units=30, activation='relu'))  
-model.add(Dense(units=30, activation='relu'))  
-model.add(Dense(units=20, activation='relu'))  
-model.add(Dense(units=20, activation='relu'))  
+
+
+
+# model.add(Dense(units=516, activation='relu'))  
+# model.add(Dense(units=516, activation='relu'))  
+model.add(Dense(units=324, activation='relu'))  
+model.add(Dense(units=324, activation='relu'))  
+model.add(Dense(units=256, activation='relu'))  
+model.add(Dense(units=256, activation='relu'))  
+model.add(Dense(units=128, activation='relu'))  
+model.add(Dense(units=128, activation='relu'))  
+model.add(Dense(units=64, activation='relu'))  
+model.add(Dense(units=64, activation='relu'))  
+# model.add(Dense(units=32, activation='relu'))  
+# model.add(Dense(units=32, activation='relu'))  
+
+
+
+# 90% 8 batch
+# model.add(Dense(units=120, activation='relu'))  
+# model.add(Dense(units=120, activation='relu'))  
+# model.add(Dense(units=110, activation='relu'))  
+# model.add(Dense(units=110, activation='relu'))  
+# model.add(Dense(units=100, activation='relu'))  
+# model.add(Dense(units=80, activation='relu'))  
+# model.add(Dense(units=80, activation='relu'))  
+# model.add(Dense(units=80, activation='relu'))  
+# model.add(Dense(units=60, activation='relu'))  
+# model.add(Dense(units=40, activation='relu'))  
+# model.add(Dense(units=30, activation='relu'))  
+# model.add(Dense(units=30, activation='relu'))  
+# model.add(Dense(units=20, activation='relu'))  
+# model.add(Dense(units=20, activation='relu'))  
+
+# 89
+# model.add(Dense(units=128, activation='relu'))  
+# model.add(Dense(units=128, activation='relu'))  
+# model.add(Dense(units=112, activation='relu'))  
+# model.add(Dense(units=112, activation='relu'))  
+# model.add(Dense(units=112, activation='relu'))  
+# model.add(Dense(units=88, activation='relu'))  
+# model.add(Dense(units=88, activation='relu'))  
+# model.add(Dense(units=88, activation='relu'))  
+# model.add(Dense(units=72, activation='relu'))  
+# model.add(Dense(units=64, activation='relu'))  
+# model.add(Dense(units=64, activation='relu'))  
+# model.add(Dense(units=32, activation='relu'))  
+# model.add(Dense(units=32, activation='relu'))  
+# model.add(Dense(units=32, activation='relu'))  
+# model.add(Dense(units=16, activation='relu'))  
 
 # Adding the output layer
 model.add(Dense(units=3, activation='softmax'))
@@ -60,7 +116,14 @@ model.add(Dense(units=3, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
 
 # Training the model on the training data
-model.fit(X_train, y_train, epochs=5, batch_size=8, verbose=1)
+model.fit(X_train, y_train, epochs=100, batch_size=8, verbose=1)
+
+# Calculate training time
+training_time = time.time() - start_time
+print("Training Time:", training_time, "seconds")
+
+######################################################################
+#evaluate
 
 # Evaluating the model on the test data
 loss = model.evaluate(X_test, y_test)
@@ -84,18 +147,18 @@ print("Confusion Matrix:")
 print(conf_matrix)
 print(f'Accuracy: {accuracy}')
 
+####################################################################
+# # single value data test
+# data_test = data.iloc[1,:-1]
 
-# single value data test
-data_test = data.iloc[1,:-1]
+# print(data_test)
+# data_test = data_test.values.reshape(1, -1)
+# print(data_test)
+# # Making a prediction for the new data point
+# prediction = model.predict(data_test)
 
-print(data_test)
-data_test = data_test.values.reshape(1, -1)
-print(data_test)
-# Making a prediction for the new data point
-prediction = model.predict(data_test)
+# # Convert prediction to class label
+# predicted_class = np.argmax(prediction, axis=1)
 
-# Convert prediction to class label
-predicted_class = np.argmax(prediction, axis=1)
-
-print("Raw Prediction:", prediction)
-print("Predicted Class:", predicted_class)
+# print("Raw Prediction:", prediction)
+# print("Predicted Class:", predicted_class)
--- svm.py
+++ svm.py
@@ -1,8 +1,13 @@
 # Importing necessary libraries
+import time
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest, f_classif, chi2, mutual_info_classif
 
 # Load data from CSV file
 data = pd.read_csv('StressLevelDataset.csv')  # Replace 'your_data.csv' with the actual file path
@@ -11,52 +16,83 @@ data = pd.read_csv('StressLevelDataset.csv')  # Replace 'your_data.csv' with the
 x = data.iloc[:,:-1]
 y = data['stress_level'].values
 
-# Splitting the data into training and testing sets (70% train, 30% test)
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+# feature selection with corelation
+# column_drop = ['self_esteem',
+#                'sleep_quality',
+#                'living_conditions',
+#                'safety',
+#                'basic_needs',
+#                'academic_performance',
+#                'teacher_student_relationship',
+#                'social_support',
+#                'headache']
+# x = x.drop(columns = column_drop)
+
+
+# Calculate the correlation matrix
+correlation_matrix = data.corr()
+
+# Print the correlation matrix
+print("Correlation Matrix:")
+print(correlation_matrix)
+
+# # Plot a heatmap of the correlation matrix
+# plt.figure(figsize=(8, 6))
+# plt.imshow(correlation_matrix, cmap='viridis', interpolation='none', aspect='auto')
+# plt.colorbar()
+# plt.xticks(range(len(correlation_matrix)), correlation_matrix.columns, rotation='vertical')
+# plt.yticks(range(len(correlation_matrix)), correlation_matrix.columns)
+# plt.title('Correlation Heatmap')
+# plt.show()
+
+#################################################################################
+
+# Splitting the data into training and testing sets (75% train, 25% test)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
+
+
+
+######################################################
+
+# # Create an LDA model
+# lda = LinearDiscriminantAnalysis(
+#     solver='eigen',           # 'svd', 'lsqr', 'eigen'
+#     shrinkage=0.5,         # float between 0 and 1 or 'auto'
+#     priors=None,            # array-like, class priors
+#     n_components=2       # int, number of components
+# )
+
+####################################################
+# lda.fit(X_train, y_train)
+
+# # Transform the training and testing data using LDA
+# X_train_lda = lda.transform(X_train)
+# X_test_lda = lda.transform(X_test)
+
+
+
+# Apply PCA for dimensionality reduction
+# pca = PCA(n_components=2)  # Adjust the number of components based on your requirements
+# X_train_pca = pca.fit_transform(X_train)
+# X_test_pca = pca.transform(X_test)
+
+
+# Create a SelectKBest object with the f_classif, chi2, and mutual_info_classif scoring function
+k_best = SelectKBest(score_func=chi2, k=9)  # Select the top 2 features
+
+# Fit and transform the training data
+X_train= k_best.fit_transform(X_train, y_train)
+
+# Transform the test data
+X_test = k_best.transform(X_test)
+
+
+#######################################################
+#model SVM
 
 # Creating an SVM model for multiclass classification
 svm_model = SVC(random_state=42)
 # svm_model = SVC(kernel='linear', C=1.0, random_state=42)
-
-######------------
-
-# # Define a parameter grid for GridSearchCV
-# param_grid = {
-#     'C': [0.1, 1, 10, 100],  # Regularization parameter
-#     'kernel': ['linear', 'rbf'],  # Kernel type
-#     'gamma': [0.01, 0.1, 1, 'scale']  # Kernel coefficient for 'rbf' kernel
-# }
-
-# # Create a GridSearchCV object
-# grid_search = GridSearchCV(estimator=svm_model, param_grid=param_grid, cv=3, scoring='accuracy')
-
-# # Training the SVM model
-# # svm_model.fit(X_train, y_train)
-# # Perform grid search to find the best hyperparameters
-# grid_search.fit(X_train, y_train)
-
-# # Get the best hyperparameters
-# best_params = grid_search.best_params_
-
-# # Making predictions with the trained model on the test data
-# # predictions = svm_model.predict(X_test)
-
-# # Use the best model for predictions
-# best_model = grid_search.best_estimator_
-# predictions = best_model.predict(X_test)
-
-# # Create a confusion matrix
-# conf_matrix = confusion_matrix(y_test, predictions)
-
-# # Calculate accuracy
-# accuracy = accuracy_score(y_test, predictions)
-
-# # Print the confusion matrix and accuracy
-# print("Confusion Matrix:")
-# print(conf_matrix)
-# print(f'Accuracy: {accuracy}')
-
-######------------
 
 # Set hyperparameter values
 C_value = 1
@@ -64,10 +100,23 @@ kernel_value = 'rbf'  # You can use 'linear' or 'rbf' or other valid kernels
 gamma_value = 'scale'  # You can use 'scale' or a numeric value
 
 # Create an SVM model with specified hyperparameters
+# param_grid = {
+#     'C': [0.1, 1, 10, 100],  # Regularization parameter
+#     'kernel': ['linear', 'rbf'],  # Kernel type
+#     'gamma': [0.01, 0.1, 1, 'scale']  # Kernel coefficient for 'rbf' kernel
+# }
+
+# Measure the training time
+start_time = time.time()
+
 svm_model = SVC(C=C_value, kernel=kernel_value, gamma=gamma_value, random_state=42)
 
 # Train the SVM model
 svm_model.fit(X_train, y_train)
+
+# Calculate training time
+training_time = time.time() - start_time
+print("Training Time:", training_time, "seconds")
 
 # Make predictions with the trained model on the test data
 predictions = svm_model.predict(X_test)
@@ -82,13 +131,16 @@ print("Confusion Matrix:")
 print(conf_matrix)
 print(f'Accuracy: {accuracy}')
 
-# single value data test
-data_test = data.iloc[0,:-1].values.reshape(1, -1)
 
-# Scale the new data point if you scaled your training data
-# new_data_point_scaled = scaler.transform(new_data_point)
+##############################################################
 
-# Making a prediction for the new data point
-prediction = svm_model.predict(data_test)
+# # single value data test
+# data_test = data.iloc[0,:-1].values.reshape(1, -1)
 
-print("Predicted Class:", prediction)
+# # Scale the new data point if you scaled your training data
+# # new_data_point_scaled = scaler.transform(new_data_point)
+
+# # Making a prediction for the new data point
+# prediction = svm_model.predict(data_test)
+
+# print("Predicted Class:", prediction)
