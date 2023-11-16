40a54da9de74466da9cc14c435e48a578d018503
manqjayak <komangjaya453@gmail.com>
2023-11-16 20:17:37 +0800

    ann and svm with stress level dataset

---
+++ ann.py
@@ -0,0 +1,82 @@
+# Importing necessary libraries
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+from tensorflow.keras.utils import to_categorical
+# Load data from CSV file
+data = pd.read_csv('StressLevelDataset.csv')  # Replace 'your_data.csv' with the actual file path
+
+print(data.info())
+print(data)
+# Assuming your CSV has columns named 'feature' and 'target'
+x = data.iloc[:,:-1]
+y = data['stress_level'].values
+print(x)
+print(y)
+
+# Convert y to one-hot encoding for multiclass classification
+y_onehot = to_categorical(y, num_classes=3)
+
+print(f"len of dependent variable {len(x.columns.tolist())}")
+# Splitting the data into training and testing sets (70% train, 30% test)
+X_train, X_test, y_train, y_test = train_test_split(x, y_onehot, test_size=0.3, random_state=42)
+
+# Creating a neural network model with 6 hidden layers
+model = Sequential()
+
+# Adding the input layer
+model.add(Dense(units=20, input_dim=X_train.shape[1], activation='relu'))  # 20 neurons in the first hidden layer
+
+# # Adding 5 hidden layers
+# for _ in range(8):
+#     model.add(Dense(units=30, activation='relu'))  # 10 neurons in each hidden layer
+model.add(Dense(units=120, activation='relu'))  
+model.add(Dense(units=120, activation='relu'))  
+model.add(Dense(units=110, activation='relu'))  
+model.add(Dense(units=110, activation='relu'))  
+model.add(Dense(units=100, activation='relu'))  
+model.add(Dense(units=80, activation='relu'))  
+model.add(Dense(units=80, activation='relu'))  
+model.add(Dense(units=80, activation='relu'))  
+model.add(Dense(units=60, activation='relu'))  
+model.add(Dense(units=40, activation='relu'))  
+model.add(Dense(units=30, activation='relu'))  
+model.add(Dense(units=30, activation='relu'))  
+model.add(Dense(units=20, activation='relu'))  
+model.add(Dense(units=20, activation='relu'))  
+
+# Adding the output layer
+model.add(Dense(units=3, activation='softmax'))
+
+# Compiling the model with mean squared error loss and Adam optimizer
+model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
+
+# Training the model on the training data
+model.fit(X_train, y_train, epochs=200, batch_size=8, verbose=1)
+
+# Evaluating the model on the test data
+loss = model.evaluate(X_test, y_test)
+print(f'Test Loss: {loss}')
+
+# Making predictions with the trained model on the test data
+predictions = model.predict(X_test)
+
+# Convert predictions to one-hot encoding for comparison
+predicted_labels = np.argmax(predictions, axis=1)
+true_labels = np.argmax(y_test, axis=1)
+
+# Create a confusion matrix
+conf_matrix = confusion_matrix(true_labels, predicted_labels)
+
+# Calculate accuracy
+accuracy = accuracy_score(true_labels, predicted_labels)
+
+# Print the confusion matrix and accuracy
+print("Confusion Matrix:")
+print(conf_matrix)
+print(f'Accuracy: {accuracy}')
---
+++ svm.py
@@ -0,0 +1,83 @@
+# Importing necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+# Load data from CSV file
+data = pd.read_csv('StressLevelDataset.csv')  # Replace 'your_data.csv' with the actual file path
+
+# Assuming your CSV has columns for features and 'target' for the dependent variable
+x = data.iloc[:,:-1]
+y = data['stress_level'].values
+
+# Splitting the data into training and testing sets (70% train, 30% test)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+# Creating an SVM model for multiclass classification
+svm_model = SVC(random_state=42)
+# svm_model = SVC(kernel='linear', C=1.0, random_state=42)
+
+######------------
+
+# # Define a parameter grid for GridSearchCV
+# param_grid = {
+#     'C': [0.1, 1, 10, 100],  # Regularization parameter
+#     'kernel': ['linear', 'rbf'],  # Kernel type
+#     'gamma': [0.01, 0.1, 1, 'scale']  # Kernel coefficient for 'rbf' kernel
+# }
+
+# # Create a GridSearchCV object
+# grid_search = GridSearchCV(estimator=svm_model, param_grid=param_grid, cv=3, scoring='accuracy')
+
+# # Training the SVM model
+# # svm_model.fit(X_train, y_train)
+# # Perform grid search to find the best hyperparameters
+# grid_search.fit(X_train, y_train)
+
+# # Get the best hyperparameters
+# best_params = grid_search.best_params_
+
+# # Making predictions with the trained model on the test data
+# # predictions = svm_model.predict(X_test)
+
+# # Use the best model for predictions
+# best_model = grid_search.best_estimator_
+# predictions = best_model.predict(X_test)
+
+# # Create a confusion matrix
+# conf_matrix = confusion_matrix(y_test, predictions)
+
+# # Calculate accuracy
+# accuracy = accuracy_score(y_test, predictions)
+
+# # Print the confusion matrix and accuracy
+# print("Confusion Matrix:")
+# print(conf_matrix)
+# print(f'Accuracy: {accuracy}')
+
+######------------
+
+# Set hyperparameter values
+C_value = 1
+kernel_value = 'rbf'  # You can use 'linear' or 'rbf' or other valid kernels
+gamma_value = 'scale'  # You can use 'scale' or a numeric value
+
+# Create an SVM model with specified hyperparameters
+svm_model = SVC(C=C_value, kernel=kernel_value, gamma=gamma_value, random_state=42)
+
+# Train the SVM model
+svm_model.fit(X_train, y_train)
+
+# Make predictions with the trained model on the test data
+predictions = svm_model.predict(X_test)
+
+# Calculate accuracy
+accuracy = accuracy_score(y_test, predictions)
+# # Create a confusion matrix
+conf_matrix = confusion_matrix(y_test, predictions)
+
+# Print the accuracy
+print("Confusion Matrix:")
+print(conf_matrix)
+print(f'Accuracy: {accuracy}')
